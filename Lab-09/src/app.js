76b7985c2b5da331173ac9ad2822b31c8422c4d7
Manav Delvadiya <24010101056@darshan.ac.in>
2026-01-02 12:48:12 +0530

    add app.js file

--- Lab-09/src/app.js
+++ Lab-09/src/app.js
@@ -7,5 +7,7 @@ export const app = express();
 app.use(express.json());
 
 import { userRoutes } from "./routes/user.routes.js";
+import { facultyRoutes } from "./routes/faculty.routes.js";
 
 app.use("/user", userRoutes);
+app.use("/faculty", facultyRoutes);
