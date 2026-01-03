8e6aa671ee1ecb970a548ca968253723011cca87
Manav Delvadiya <24010101056@darshan.ac.in>
2026-01-03 19:46:11 +0530

    Add Faculty model file

---
+++ Lab-09/src/model/faculty.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const facultySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lower: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Faculty = mongoose.model("Faculty", facultySchema);
